81b55c6eee1545257d75c3beb789bd17caa55376
RahmadFadhil8 <rahmadfadhil252@gmail.com>
2026-06-05 15:18:03 +0700

    menambah hover pada tombol

--- app/components/ui/Button.vue
+++ app/components/ui/Button.vue
@@ -1,6 +1,6 @@
 <template>
     <NuxtLink to="#Paket">
-        <div class="px-4 py-2 bg-[#249CD9]  rounded-lg w-fit">
+        <div class="px-4 py-2 bg-[#249CD9] hover:bg-[#0081c2]  rounded-lg w-fit">
             <p class="font-bold text-white">Daftar Sekarang</p>
         </div>
     </NuxtLink>
