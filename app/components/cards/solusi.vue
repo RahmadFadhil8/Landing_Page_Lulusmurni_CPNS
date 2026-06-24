cdc9e081433dcd625b78fcca926e9faa82267e42
RahmadFadhil8 <rahmadfadhil252@gmail.com>
2026-06-24 11:09:29 +0700

    memperbaharui section solusi

--- app/components/cards/solusi.vue
+++ app/components/cards/solusi.vue
@@ -1,16 +1,84 @@
 <template>
     <UiCard variant="Solusi">
-        <div class="max-w-sm w-full p-1 flex flex-col">    
+        <div class="w-full p-1 flex flex-col">    
             <div class="h-full p-8 flex flex-col gap-5">
-                <div class="flex flex-col gap-2">
-                    <h5 class="font-bold text-lg text-blue-950 leading-tight">
-                    {{ Judul }}
-                    </h5>
+                <div v-if="image || image2" class="flex flex-col lg:flex-row gap-2">
+                    <div class="shrink-0">
+                        <img 
+                            v-if="image2"
+                            :src="image2" 
+                            :alt="Judul"
+                            class="rounded-xl w-full object-cover hidden lg:block"
+                        >
+                    </div>
+
+                    <div v-if="image" class="flex-1 mr-4">
+                        <div class="flex items-center gap-4">
+                            <img v-if="icon" :src="icon" :alt="Judul" class="h-6 w-6">
+                            <h5 class="font-bold text-lg text-blue-950 leading-tight">
+                            {{ Judul }}
+                            </h5>
+                        </div>
+
+                        <div class="w-16 h-1 bg-black rounded-full mt-1 mb-2"></div>
+                        
+                        <p class="text-sm leading-relaxed font-light mb-4">
+                            {{ SubJudul }}
+                        </p>
+                        
+                        <UiButton v-if="button" class="font-bold text-white">
+                            {{ buttonText }}
+                        </UiButton>
+                    </div>
+
+                    <div v-else class="flex-1 ml-4">
+                        <div class="flex items-center gap-4">
+                            <img v-if="icon" :src="icon" :alt="Judul" class="h-6 w-6">
+                            <h5 class="font-bold text-lg text-blue-950 leading-tight">
+                            {{ Judul }}
+                            </h5>
+                        </div>
+
+                        <div class="w-16 h-1 bg-black rounded-full mt-1 mb-2"></div>
+                        
+                        <p class="text-sm leading-relaxed font-light mb-4">
+                            {{ SubJudul }}
+                        </p>
+                        
+                        <UiButton v-if="button" class="font-bold text-white">
+                            {{ buttonText }}
+                        </UiButton>
+                    </div>
                     
-                    <div class="w-16 h-1 bg-black rounded-full mt-1 mb-2"></div>
-                    
+                    <div class="shrink-0">
+                        <img 
+                            v-if="image"
+                            :src="image" 
+                            :alt="Judul"
+                            class="rounded-xl w-full object-cover hidden lg:block"
+                        >
+                    </div>
+                </div>
+
+                <!-- Card biasa tanpa gambar -->
+                <div v-else>
+                    <div class="flex items-center gap-4 mb-4">
+                        <img
+                            v-if="icon"
+                            :src="icon"
+                            :alt="Judul"
+                            class="h-6 w-6"
+                        >
+
+                        <h5 class="font-bold text-lg text-blue-950">
+                            {{ Judul }}
+                        </h5>
+                    </div>
+
+                    <div class="w-16 h-1 bg-black rounded-full mb-4"></div>
+
                     <p class="text-sm leading-relaxed font-light">
-                    {{ SubJudul }}
+                        {{ SubJudul }}
                     </p>
                 </div>
             </div>
@@ -22,5 +90,11 @@
 defineProps({
   Judul: String,
   SubJudul: String,
+  span: String,
+  button: Boolean,
+  image: String,
+  image2: String,
+  icon: String,
+  buttonText: String
 })
 </script>
--- app/components/sections/SolusiCPNS.vue
+++ app/components/sections/SolusiCPNS.vue
@@ -3,37 +3,20 @@
         <div class="px-14 lg:px-48">
             <div class="max-w-2xl lg:max-w-3xl mx-auto">
                 <h4 class="text-center font-bold text-2xl lg:text-3xl mb-4">Kami Membantu Kamu Belajar dengan Lebih Terarah</h4>
-                <p class="mb-4 text-center">Persiapan CPNS bukan hanya soal banyak belajar, tetapi juga memahami kemampuan diri, membiasakan diri dengan sistem CAT, dan berlatih menggunakan soal yang sesuai standar terbaru.</p>
+                <p class="mb-8 text-center">Persiapan CPNS bukan hanya soal banyak belajar, tetapi juga memahami kemampuan diri, membiasakan diri dengan sistem CAT, dan berlatih menggunakan soal yang sesuai standar terbaru.</p>
             </div>
-            <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8">
-                <div class="text-center lg:text-left">
-                    <div class="flex justify-center lg:justify-start">
-                        <UiButton class="font-bold text-white">
-                            Daftar Sekarang
-                        </UiButton>
-                    </div>
-                </div>
-                <div class="hidden lg:flex lg:justify-end items-end">
-                    <CardsSolusi 
-                    v-for="item in Solusi1"
-                    :Judul="item.Judul"
-                    :SubJudul="item.SubJudul"
-                    />
-                </div>
-            </div>
-            <div class="flex justify-center items-center">
-                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-                    <CardsSolusi 
-                    v-for="item in Solusi2"
-                    :Judul="item.Judul"
-                    :SubJudul="item.SubJudul"
-                    />
-                    <CardsSolusi 
-                        v-for="item in Solusi1"
-                        :Judul="item.Judul"
-                        :SubJudul="item.SubJudul"
-                         class="lg:hidden"/>
-                </div>
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-8 mb-8">
+                <CardsSolusi 
+                v-for="item in Solusi1"
+                :Judul="item.Judul"
+                :SubJudul="item.SubJudul"
+                :button="item.button"
+                :buttonText="item.buttonText"
+                :icon="item.icon"
+                :image="item.image"
+                :image2="item.image2"
+                :class="item.span"
+                />
             </div>
         </div>
     </section>
@@ -41,26 +24,37 @@
 
 <script setup>
 import { ref } from 'vue';
+import char from '~/assets/icon/chart.svg'
+import book from '~/assets/icon/book.svg'
+import trophy from '~/assets/icon/trophy.svg'
+import CAT from '~/assets/images/CAT.jpg'
 
 const Solusi1 = ref([
     {
         Judul:"Simulasi CAT dengan Batas Waktu Realistis",
-        SubJudul:"Biasakan diri mengerjakan soal dalam kondisi yang menyerupai ujian sebenarnya sehingga lebih siap mengelola waktu saat tes berlangsung."
-    },
-])
+        SubJudul:"Biasakan diri mengerjakan soal dalam kondisi yang menyerupai ujian sebenarnya sehingga lebih siap mengelola waktu saat tes berlangsung.",
+        span: "lg:col-span-2",
 
-const Solusi2 = ref([
+        button: true,
+        buttonText: "Coba Sekarang",
+        image:CAT
+    },
     {
         Judul:"Penilaian Otomatis dan Evaluasi Hasil",
-        SubJudul:"Lihat hasil tryout secara langsung untuk mengetahui sejauh mana kesiapanmu menghadapi SKD."
+        SubJudul:"Lihat hasil tryout secara langsung untuk mengetahui sejauh mana kesiapanmu menghadapi SKD.",
+        icon: char
     },
     {
         Judul:"Soal Terupdate Sesuai Standar Terbaru",
-        SubJudul:"Latihan menggunakan soal yang disusun mengikuti pola dan tingkat kesulitan terbaru."
+        SubJudul:"Latihan menggunakan soal yang disusun mengikuti pola dan tingkat kesulitan terbaru.",
+        icon: book
     },
     {
         Judul:"Pengalaman Tryout yang Mendekati Ujian Asli",
-        SubJudul:"Membantu meningkatkan rasa percaya diri melalui simulasi yang dirancang mendekati ujian asli."
+        SubJudul:"Membantu meningkatkan rasa percaya diri melalui simulasi yang dirancang mendekati ujian asli.",
+        span: "lg:col-span-2",
+        icon: trophy,
+        image2:CAT
     },
 ])
 </script>
